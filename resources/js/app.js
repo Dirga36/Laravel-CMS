211ce908eb652039caae4a5ac1e295bed207c526
Dirge <fuaddabbad@gmail.com>
2025-02-22 18:09:22 +0700

    Add new Blade components and update dependencies; refactor app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,8 @@
 import './bootstrap';
 import 'flowbite';
-import Quill from 'quill';
 
-// Quill
-import "quill/dist/quill.core.css";
-/* 
-* Or if you only need the core build
-* import Quill from 'quill/core';
-*/
-const quill = new Quill('#editor');
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
